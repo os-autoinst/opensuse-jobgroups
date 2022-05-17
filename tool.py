2dd620e7ad7e6d6f4b2abe2a8057d84636fd0483
Dominik Heidler <dheidler@suse.de>
2022-05-17 16:54:10 +0200

    CI: Don't encode : and ,

--- tool.py
+++ tool.py
@@ -64,7 +64,7 @@ def api_request(*args):
 
 
 def github_workflow_encode(s):
-	return s.replace('%', '%25').replace('\r', '%0D').replace('\n', '%0A').replace(':', '%3A').replace(',', '%2C')
+	return s.replace('%', '%25').replace('\r', '%0D').replace('\n', '%0A')
 
 
 def show_server_error(r, filename=""):
